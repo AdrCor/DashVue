3c2479f8d76674fac3dc974fee45ca58b65ece86
AdrCor <adrien.corre1999@gmail.com>
2023-07-17 00:38:15 +0200

    Refactor cards folder

--- src/components/cards/index.ts
+++ src/components/cards/index.ts
@@ -1,7 +1,7 @@
-import CardLayout from "./CardLayout.vue";
-import CardHeader from "./sub/Header.vue";
-import CardSub from "./sub/Sub.vue";
-import CardValue from "./sub/Value.vue";
+import CardLayout from "./Layout.vue";
+import CardHeader from "./Header.vue";
+import CardSub from "./Sub.vue";
+import CardValue from "./Value.vue";
 
 export {
     CardLayout,
--- src/components/list/items/RankItem.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <p class="mb-0.5 text-center text-xl text-front text-opacity-60">{{ rank }}.</p>
-</template>
-
-<script setup lang="ts">
-defineProps<{
-    rank: number
-}>()
-</script>
