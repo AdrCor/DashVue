b04d73948863b3f7a6907cb02805ac9ae72dbd1e
AdrCor <adrien.corre1999@gmail.com>
2023-07-16 23:38:31 +0200

    Add card visits

---
+++ src/blocs/CardVisits.vue
@@ -0,0 +1,29 @@
+<template>
+    <CardLayout class="min-h-96 flex w-full flex-col justify-between gap-4">
+        <CardHeader header="Website visits" sub="Last 7 days" />
+
+        <div class="flex flex-row items-center justify-between gap-4">
+            <CardValue
+                class="self-start"
+                :value="127654"
+                prefix="$"
+                color="primary"
+                :sub-value="-13142"
+                sub-text="vs previous period"
+                :gain="-11"
+            />
+            <Button color="neutral" size="small" class="self-end">
+                <template #icon>
+                    <ChevronRight />
+                </template>
+                More
+            </Button>
+        </div>
+    </CardLayout>
+</template>
+
+<script setup lang="ts">
+import { CardLayout, CardHeader, CardValue } from '@/components/cards'
+import Button from '@/components/buttons/Button.vue'
+import ChevronRight from '@/components/icons/outline/ChevronRight.vue';
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,10 @@
 <template>
     <div :key="width" class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-        <CardRevenue />
-        <CardSales />
-        <CardRevenue />
-        <CardRevenue />
-        <CardPayments class="md:col-span-2 overflow-auto"/>
+        <CardVisits />
+        <CardVisits class="lg:row-start-2"/>
+        <CardRevenue class="lg:row-span-2"/>
+        <CardSales class="lg:row-span-2"/>
+        <CardPayments class="md:col-span-2 lg:col-span-3 overflow-auto"/>
     </div>
 </template>
 
@@ -13,6 +13,7 @@ import CardRevenue from '@/blocs/CardRevenue.vue';
 import CardPayments from '@/blocs/CardPayments.vue'
 import { useWindowSize } from '@vueuse/core';
 import CardSales from '@/blocs/CardSales.vue';
+import CardVisits from '@/blocs/CardVisits.vue';
 
 const { width } = useWindowSize()
 
