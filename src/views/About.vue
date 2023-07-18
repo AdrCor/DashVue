92ae3aa581ae2c1c9bbc8c39e0fd2c29f488ea7b
AdrCor <adrien.corre1999@gmail.com>
2023-07-18 03:16:36 +0200

    Add about text

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,3 +1,24 @@
 <template>
-    <div>about</div>
+    <CardLayout class="flex flex-col gap-4">
+        <div>
+            <h2 class="text-primary text-2xl font-medium">DashVue</h2>
+            <p class="text-neutral text-justify">
+                DashVue is an open-source template that allows you to create responsive and modern-looking dashboards using Vue.js and TailwindCSS.
+            </p>
+        </div>
+        <div>
+            <h2 class="text-primary text-2xl font-medium">What's included?</h2>
+            <ol class="mx-2">
+                <li>- Custom design system that supports dark and light modes out of the box and can be easily customized and expanded.</li>
+                <li>- Responsive navigation bars for desktop and mobile.</li>
+                <li>- Basic routing system using vue-router.</li>
+                <li>- Basic store setup using Pinia.</li>
+                <li>- Multiple components and example cards.</li>
+            </ol>
+        </div>
+    </CardLayout>
 </template>
+
+<script setup lang="ts">
+import { CardLayout } from '@/components/cards';
+</script>
